aea2a2a21b1a673898000b5ca44e644e804c4b1a
FilipLac <93822443+FilipLac@users.noreply.github.com>
2023-04-21 23:56:27 +0200

    Added Licence file and modified some values before first build.

--- project/Form1.cs
+++ project/Form1.cs
@@ -7,7 +7,7 @@ namespace English_Exam_Timer
 {
     public partial class Form1 : Form
     {
-        //Version 1.0.1
+        //Version 1.1.0
         //Developed by: Filip Lacina
 
         //Inicializing private variables
